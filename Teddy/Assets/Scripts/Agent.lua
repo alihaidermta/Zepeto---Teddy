9b7ef2b1b8933aaf485173816a08efd52c83b528
HamzaAziz786 <98694278+HamzaAziz786@users.noreply.github.com>
2024-04-29 21:43:25 +0500

    teddy indicator

--- Teddy/Assets/Scripts/Agent.lua
+++ Teddy/Assets/Scripts/Agent.lua
@@ -1,5 +1,17 @@
 --!SerializeField
 local particles : ParticleSystem = nil
+--!SerializeField
+local indicator : GameObject = nil
+
+
+function self:Start()
+    if(self.tag=="agent") then
+        indicator.SetActive(indicator, true)
+    end
+end
+
+
+
 function self:OnTriggerEnter(collider)
     print("agent trigger ho gya ")
     if(collider.tag == "agent") then
